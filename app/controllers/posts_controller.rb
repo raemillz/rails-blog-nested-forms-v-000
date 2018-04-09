08a13e176602e499650b855af16324f27f435688
Rachel Miller <raeanne1991@gmail.com>
2018-04-09 16:19:24 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,3 @@
-
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
@@ -70,7 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:name, :content, :tag_ids => [], :tags_attributes => [:name])
-
+      params.require(:post).permit(:name, :content, :tag_ids => [], tags_attributes: [:name])
     end
 end
