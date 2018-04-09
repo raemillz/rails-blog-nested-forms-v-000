72a9b7ac33aa62b7cb8ae727ebd76a3a094c356b
Rachel Miller <raeanne1991@gmail.com>
2018-04-09 16:21:40 +0000

    automatically backed up by learn

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,26 +1,6 @@
-
-class Post < ActiveRecord::Base
-  belongs_to :user
-  has_many :comments
+class Tag < ActiveRecord::Base
   has_many :post_tags
-  has_many :tags, :through => :post_tags
-  validates_presence_of :name, :content
+  has_many :posts, :through => :post_tags
 
-  accepts_nested_attributes_for :tags, reject_if: proc {|tag_attributes| tag_attributes['name'].blank?}
-
-
-# Question: if I wanted to use a  custom writer, how would I add a reject_if blank?
-
-  # def tags_attributes=(tags_attributes)
-  # 	tags_attributes.values.each do |name|
-  #   		tag= Tag.find_or_create_by(name)
-  # 		self.tags << tag
-  # 	end
-  # end
-
-
-# Something like this?
-  # def is_blank
-  #   tags_attributes.values.blank?
-  # end
+  validates_uniqueness_of :name
 end
