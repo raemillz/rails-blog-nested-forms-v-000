bfeb819317aae1d715364d4efbfcb6e2863daba3
Rachel Miller <raeanne1991@gmail.com>
2018-04-09 16:19:43 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,11 @@
+
 class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments
   has_many :post_tags
   has_many :tags, :through => :post_tags
+
   validates_presence_of :name, :content
 
-  accepts_nested_attributes_for :tags, reject_if: proc {|tag_attributes| tag_attributes['name'].blank?}
-
+  accepts_nested_attributes_for :tags, reject_if: proc { |attributes| attributes['name'].blank? }
 end
